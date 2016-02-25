f62f695bc810471bff9e54e6cbed5879d27bb079
Claire <ClaireS29@gmail.com>
2016-02-25 10:45:03 -0800

    added code to display a d3 visualization of just carbon emissions. mostly working but needs some further understanding

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -5,13 +5,15 @@ class MapsController < ApplicationController
   LONG = 37.871667
 
   def map
-    @name = get_balancing_authority(LAT, LONG)
-    ba = get_abbrev_by_location(LAT, LONG)
+    # @name = get_balancing_authority(LAT, LONG)
+    # ba = get_abbrev_by_location(LAT, LONG)
+    #
+    # response = HTTParty.get("https://api.watttime.org:443/api/v1/datapoints/?ba=#{ba}&page_size=1", headers={'Authorization': "Token #{ENV['WATT_TIME_TOKEN']}"})
+    # @carbon = response["results"][0]["carbon"]
+    # @genmix = response["results"][0]["genmix"]
 
-    response = HTTParty.get("https://api.watttime.org:443/api/v1/datapoints/?ba=#{ba}&page_size=1", headers={'Authorization': "Token #{ENV['WATT_TIME_TOKEN']}"})
-    binding.pry
-    @carbon = response["results"][0]["carbon"]
-    @genmix = response["results"][0]["genmix"]
+    response = HTTParty.get("https://api.watttime.org:443/api/v1/datapoints/?ba=BPA&start_at=2016-02-23&freq=5m&market=RT5M", headers={'Authorization': "Token #{ENV['WATT_TIME_TOKEN']}"})
+    @response = response["results"]
 
   end
 
