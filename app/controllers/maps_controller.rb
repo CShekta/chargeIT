1c3d1d70e6abc6b3e89312f56f0cdc2cc3c8d33d
Claire <ClaireS29@gmail.com>
2016-03-01 11:46:37 -0800

    separated out fuel_sources method

--- app/controllers/maps_controller.rb
+++ app/controllers/maps_controller.rb
@@ -3,19 +3,17 @@ class MapsController < ApplicationController
   include HTTParty
   LAT = -122.272778
   LONG = 37.871667
+  BASE_URI = "https://api.watttime.org:443/api/v1"
 
   def map
     gon.stations = Station.all
-    ba = get_ba(LAT, LONG)
-    start_time = (Time.now - 2.day).strftime("%Y-%m-%d")
+    # ba = get_ba(params[:lat], params[:lng])
     #
     # response = HTTParty.get("https://api.watttime.org:443/api/v1/datapoints/?ba=#{ba}&page_size=1", headers={'Authorization': "Token #{ENV['WATT_TIME_TOKEN']}"})
+    @response = get_fuel_data
 
-    response = HTTParty.get("https://api.watttime.org:443/api/v1/datapoints/?ba=BPA&start_at=#{start_time}&freq=5m&market=RT5M", headers={'Authorization': "Token #{ENV['WATT_TIME_TOKEN']}"})
-    @response = response["results"]
 
-    # response = HTTParty.get("https://api.watttime.org:443/api/v1/marginal/?ba=BPA&start_at=2016-02-23&freq=5m&market=RT5M", headers={'Authorization': "Token #{ENV['WATT_TIME_TOKEN']}"})
-    # binding.pry
+    # response = HTTParty.get("#{BASE_URI}/marginal/?ba=BPA&start_at=2016-02-23&freq=5m&market=RT5M", headers={'Authorization': "Token #{ENV['WATT_TIME_TOKEN']}"})
     # @response = response["results"]
   end
 
@@ -24,7 +22,7 @@ class MapsController < ApplicationController
   end
 
   def graph_test
-    response = HTTParty.get("https://api.watttime.org:443/api/v1/datapoints/?ba=BPA&start_at=2016-02-27&freq=5m&market=RT5M", headers={'Authorization': "Token #{ENV['WATT_TIME_TOKEN']}"})
+    response = HTTParty.get("#{BASE_URI}/datapoints/?ba=BPA&start_at=2016-02-27&freq=5m&market=RT5M", headers={'Authorization': "Token #{ENV['WATT_TIME_TOKEN']}"})
     @response = response["results"]
   end
 
@@ -32,13 +30,19 @@ class MapsController < ApplicationController
     gon.stations = Station.all
   end
 
+  def get_fuel_data
+    start_time = (Time.now - 2.day).strftime("%Y-%m-%d")
+    response = HTTParty.get("#{BASE_URI}/datapoints/?ba=BPA&start_at=#{start_time}&freq=5m&market=RT5M", headers={'Authorization': "Token #{ENV['WATT_TIME_TOKEN']}"})
+    response = response["results"]
+  end
+
   def get_balancing_authority(lat,long)
-    response = HTTParty.get("https://api.watttime.org/api/v1/balancing_authorities/?loc={'type':'Point','coordinates':[#{lat},#{long}]}")
+    response = HTTParty.get("#{BASE_URI}/balancing_authorities/?loc={'type':'Point','coordinates':[#{lat},#{long}]}")
     @name = response[0]["name"]
   end
 
   def get_ba(lat,long)
-    response = HTTParty.get("https://api.watttime.org/api/v1/balancing_authorities/?loc={'type':'Point','coordinates':[#{lat},#{long}]}")
+    response = HTTParty.get("#{BASE_URI}/balancing_authorities/?loc={'type':'Point','coordinates':[#{lat},#{long}]}")
     abbrev = response[0]["abbrev"]
   end
 
