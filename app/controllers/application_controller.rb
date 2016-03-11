59d617edeb226c6a4040df22b6d281c2cf7c8180
Claire <ClaireS29@gmail.com>
2016-03-11 14:14:23 -0800

    deleted files made unneccesary after devise gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,4 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
-
-  # def current_user
-  #   @current_user ||= User.find(session[:user_id]) if session[:user_id]
-  # end
-  #
-  # def require_login
-  #   if current_user.nil?
-  #     flash[:error] = "You must be logged in to view this section"
-  #     redirect_to login_path
-  #   end
-  # end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,4 @@
 class UsersController < ApplicationController
-  # def new
-  #   @user = User.new
-  # end
-  #
-  # def create
-  #   @user = User.new(user_params)
-  #   if @user.save
-  #     @user.latitude = @user.zip_code.to_lat
-  #     @user.longitude = @user.zip_code.to_lon
-  #     @user.save
-  #     redirect_to new_session_path
-  #   else
-  #     render "new"
-  #   end
-  # end
-
   def show
     @user = User.find(current_user.id)
   end
